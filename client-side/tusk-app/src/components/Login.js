fe8c48d03b4bf97b4e459372729c5383f9538e97
Anthony DiBlasio <anthony.diblasio11@gmail.com>
2023-11-09 14:21:20 -0500

    fixing a login bug

--- client-side/tusk-app/src/components/Login.js
+++ client-side/tusk-app/src/components/Login.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
@@ -12,42 +13,20 @@ const Login = () => {
       const { user, token } = response.data;
       localStorage.setItem('token', token);
       localStorage.setItem('userId', user.id);
-      // Redirect to the user's specific profile page
+      navigate(`/profile/${user.id}`);
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
- 
-    <div >
-    <div className='container' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto', marginTop: '10rem' }}>
-      <h1 style={{ marginBottom: '1rem' }}>Tusk Login</h1>
-    
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        style={{ padding: '0.5rem', marginBottom: '1rem' }}
-      />
-    
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        style={{ padding: '0.5rem', marginBottom: '1rem' }}
-      />
-      <button style={{ padding: '0.5rem 2rem', borderRadius: '0.25rem', background: '#007bff', color: 'white' }} onClick={handleLogin}>
-        Login
-      </button>
-    </div>
-    </div>
+    <form className='container' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 'auto', marginTop: '10rem' }}>
+      <h1>User Login</h1>
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} autoComplete="current-password" />
+      <button type="button" onClick={handleLogin}>Login</button>
+    </form>
   );
 };
 
 export default Login;
-
-
-
--- client-side/tusk-app/src/components/NavTab.js
+++ client-side/tusk-app/src/components/NavTab.js
@@ -1,52 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 import axios from 'axios';
-import { useNavigate } from "react-router-dom";
-import tusklogo from '../pics/tusklogo.svg'
+import tusklogo from '../pics/tusklogo.svg';
+
 const NavTab = () => {
-    const navigate = useNavigate();
-    const loggedIn = !!localStorage.getItem('token');
-  
-    const handleLogout = async () => {
-      try {
-        const response = await axios.get('http://localhost:3001/api/users/logout');
-        console.log(response.data);
-  
-        localStorage.removeItem('token');
-        localStorage.clear();
-        navigate('/');
-      } catch (error) {
-        console.error('Logout error:', error);
-      }
-    };
+  const navigate = useNavigate();
+  const loggedIn = !!localStorage.getItem('token');
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/api/users/logout');
+      console.log(response.data);
+
+      localStorage.removeItem('token');
+      navigate('/');
+    } catch (error) {
+      console.error('Logout error:', error);
+    }
+  };
 
   return (
     <Navbar expand="lg" variant="light" bg="light">
-      <Navbar.Brand>
-        <Link to="/">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/">
           <img
             style={{ height: '80px' }}
             src={tusklogo}
             alt="logo"
             className="nav-logo"
           />
-        </Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
-        <Nav>
+          </Nav.Link>
+        </Nav>
+        <Nav className='nav-links'>
           {loggedIn ? (
             <>
               <Nav.Link as={Link} to={`/profile/${localStorage.getItem('userId')}`}>
                 Profile
               </Nav.Link>
-              <Nav.Link onClick={() => {
-             handleLogout();
-                }}>
-                Logout
-              </Nav.Link>
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
             </>
           ) : (
             <>
@@ -58,7 +52,6 @@ const NavTab = () => {
               </Nav.Link>
             </>
           )}
-        </Nav>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
